35241f1a892abda3a39b463077493910a0060407
Maria Catharina van Veen <maria.vanVeen@Libertyserver.com>
2020-09-10 14:21:19 -0400

    Create One-Page-Website
    Create One-Page-Website plus add pictures for future use of quilting page/site

---
+++ HACC/One-Page-Website/scripts.js
@@ -0,0 +1,7 @@
+var strVar="";
+strVar += "			<a href=\"#home\">HOME<\/a> |";
+strVar += "			<a href=\"#gallery\">GALLERY<\/a> |";
+strVar += "			<a href=\"#add_quilt\">ADD QUILT<\/a> |";
+strVar += "			<a href=\"#tutorials\">TUTORIALS<\/a> |";
+
+document.getElementById("navbar").innerHTML = strVar;
