2fcadcdaadab134eba1a5fd6025e3fc83df898e5
Maria Catharina van Veen <maria.vanVeen@Libertyserver.com>
2020-09-10 12:15:35 -0400

    Script for NAV, Special fonts in CSS

---
+++ HACC/Assignments/scripts.js
@@ -0,0 +1,8 @@
+var strVar="";
+strVar += "			<a href=\"Assingment.html\">HOME<\/a> |";
+strVar += "			<a href=\"Form.html\">FORM<\/a> |";
+strVar += "			<a href=\"Dog.html\">DOG PICTURE<\/a> |";
+strVar += "			<a href=\"Dog_Video.html\">DOG VIDEO<\/a> |";
+strVar += "			<a href=\"https:\/\/www.google.com\/\">Google Link<\/a>";
+
+document.getElementById("navbar").innerHTML = strVar;
